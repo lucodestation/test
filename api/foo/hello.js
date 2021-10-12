20641e7ece41fac586d607fd9dec3811d2507da6
卢忠伟 <lucodestation@163.com>
2021-10-12 23:20:06 +0800

    测试

--- api/foo/hello.js
+++ api/foo/hello.js
@@ -1,6 +1,3 @@
 module.exports = (req, res) => {
-  if (req.method === "POST") {
-    res.status(200).send("post: <h1>Hello World</h1>");
-  }
-  res.status(200).send("<h1>Hello World</h1>");
+  res.status(200).send(req.method + " <h1>Hello World</h1>");
 };
