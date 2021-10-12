f042d69623e125b8ec695ca4246cdb63f1d61b1c
卢忠伟 <lucodestation@163.com>
2021-10-13 00:58:02 +0800

    1

--- api/index.js
+++ api/index.js
@@ -2,10 +2,11 @@ const app = require("express")();
 
 const router = require("./router");
 
-app.use("/api", router);
-
 app.get("/foo", (req, res, next) => {
   res.send("foo");
 });
+
+router.use("/user", require("./user"));
+router.use("/info", require("./info"));
 
 module.exports = app;
