961888954d14d23846163f5a6fd090c31b2daeb3
卢忠伟 <lucodestation@163.com>
2021-10-29 01:18:15 +0800

    test

--- api/router/index.js
+++ api/router/index.js
@@ -9,7 +9,7 @@ router.use("/user", require("../user.js")); // 不行
 router.use("/info", info); // 不行
 router.use("/article", require("../article/index.js"));
 router.use("/article2", require("./article2.js"));
-router.use("/user2", require("./user2.js"));
+router.use("/info2", require("./info2.js"));
 
 router.get("/", (req, res, next) => {
   res.send("api");
