91eddcc188555d58176c8e0acb3762034bed0932
卢忠伟 <lucodestation@163.com>
2021-10-29 00:48:45 +0800

    移动 router.js 位置

--- api/router/index.js
+++ api/router/index.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router(); // 路由实例，相当于一个 mini Express 实例
 
 // const user = require("./user");
-const info = require("./info");
+const info = require("../info");
 
-router.use("/user", require("./user")); // 不行
+router.use("/user", require("../user")); // 不行
 router.use("/info", info); // 不行
-router.use("/article", require("./article"));
+router.use("/article", require("../article"));
 
 router.get("/", (req, res, next) => {
   res.send("api");
