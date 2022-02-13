1b87234d53257abc06df0321b578be51ae7d5c80
卢忠伟 <lucodestation@163.com>
2022-02-13 22:36:49 +0800

    add cors

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
-const app = require("express")();
+const app = require('express')()
+const cors = require('cors')
 
-const router = require("./router");
+const router = require('./router')
+app.use(cors()) // 允许跨域
 
-app.use("/api", router);
+app.use('/api', router)
 
 app.get('/', (req, res, next) => {
   res.status(200).send('Hello World!')
 })
 
-app.get("/foo", (req, res, next) => {
-  res.send("foo");
-});
-app.post("/foo", (req, res, next) => {
-  res.send("POST foo");
-});
+app.get('/foo', (req, res, next) => {
+  res.send('foo')
+})
+app.post('/foo', (req, res, next) => {
+  res.send('POST foo')
+})
 
-module.exports = app;
+module.exports = app
